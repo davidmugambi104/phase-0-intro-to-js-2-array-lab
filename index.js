d2b96777dd06bcf21d4bfcf7e02420c97d9f578f
davidmugambi104 <davidmugambi104@gmail.com>
2024-03-23 17:22:17 -0400

    array lab

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Write your solution here!
+const cats =[
+   "Milo", "Otis", "Garfield"]
+
+ function destructivelyAppendCat(name) {
+cats.push(name);
+ }
+ function destructivelyPrependCat(name) {
+   cats.unshift(name); }
+ 
+ function destructivelyRemoveLastCat() {
+   cats.pop()
+ }
+ function destructivelyRemoveFirstCat() {
+   cats.shift()
+ }
+
+ function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+// Function to remove the first cat from the cats array
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+// Function to append a cat to the cats array and return a new array
+function appendCat(name) {
+    let newCats = cats.concat(name);
+    return newCats;
+}
+
+// Function to prepend a cat to the cats array and return a new array
+function prependCat(name) {
+    let newCats = [name, ...cats];
+    return newCats;
+}
+
+// Function to remove the last cat in the cats array and return a new array
+function removeLastCat() {
+    let newCats = cats.slice(0, -1);
+    return newCats;
+}
+
+// Function to remove the first cat from the cats array and return a new array
+function removeFirstCat() {
+    let newCats = cats.slice(1);
+    return newCats; }
